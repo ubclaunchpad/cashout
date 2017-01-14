97a149768bd1ec28c9a60ef1a312f519042e115a
Bruno Bachmann <bfbachmann@gmail.com>
2017-01-14 13:09:37 -0800

    Add Devise for user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :transaction
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
