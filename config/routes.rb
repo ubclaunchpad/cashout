5dbd47e133c98d6d57f1e7561405828cb2b4ed65
Bruno Bachmann <bfbachmann@gmail.com>
2017-01-14 13:40:58 -0800

    Add link to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   get 'home/index'
 
+  # Automatically sets up Devise-specific routes
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # This is the root url
   root to: "home#index"
