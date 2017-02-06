b739944fe466a8cd5f483dda25f8c7bdf3f5fddf
Pasindu <pasindumuth@gmail.com>
2017-02-06 09:16:30 -0800

    Purchase submit redirects to wrong url: fixed

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -26,7 +26,7 @@ class PurchasesController < ApplicationController
 
         respond_to do |format|
             if @purchase.save
-                format.html { redirect_to @purchase, notice: 'Purchase was successfully created.' }
+                format.html { redirect_to purchases_path, notice: 'Purchase was successfully created.' }
                 format.json { render :show, status: :created, location: @purchase }
             else
                 format.html { render :new }
